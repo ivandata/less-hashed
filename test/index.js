e855e20d07fc7a5689c3cb4019cf71c5a8e49477
Ivandata <ivandata@users.noreply.github.com>
2017-07-25 01:41:58 +0300

    Tests for HASH_PATH and HASH_NAME

--- test/index.js
+++ test/index.js
@@ -4,55 +4,95 @@ import {expect, assert} from 'chai';
 import lessHashed from '../lib/index';
 import fs from 'fs-extra';
 
-const stubs = `${process.cwd()}/test/stubs/`;
+const stubs = `${process.cwd()}/test/stubs`;
 
 describe("Index", function () {
 
-  let less_files_path = `${stubs}extensions/**/*.less`;
-  let hash_file_path = `${stubs}hashes.json`;
+  let less_files_path = `${stubs}/extensions/**/*.less`;
+  let hash_file_path = `${stubs}/`;
+  let hash_file_name = 'hashes.json';
 
-  context('When save_sources_hashes_file is false', () => {
-    let options = {
-      save_hash_file: false
-    };
+  context('Without options', () => {
 
     it("return files to compile", async () => {
-      let result = await lessHashed(less_files_path, hash_file_path, options);
+      let result = await lessHashed(less_files_path);
       expect(result)
         .to.eql([
-          `${stubs}extensions/css-imports/file.less`,
-          `${stubs}extensions/empty-imports/file.less`,
-          `${stubs}extensions/empty-imports/imports/import-file.less`,
-          `${stubs}extensions/less-imports/file.less`,
-          `${stubs}extensions/less-imports/imports/import-file.less`,
-          `${stubs}extensions/php-imports/file.less`,
-          `${stubs}extensions/set-imports/file.less`,
-          `${stubs}extensions/set-imports/imports/second-import-file.less`,
-          `${stubs}extensions/set-imports/imports/third-import-file.less`
+        `${stubs}/extensions/css-imports/file.less`,
+        `${stubs}/extensions/empty-imports/file.less`,
+        `${stubs}/extensions/empty-imports/imports/import-file.less`,
+        `${stubs}/extensions/less-imports/file.less`,
+        `${stubs}/extensions/less-imports/imports/import-file.less`,
+        `${stubs}/extensions/php-imports/file.less`,
+        `${stubs}/extensions/set-imports/file.less`,
+        `${stubs}/extensions/set-imports/imports/second-import-file.less`,
+        `${stubs}/extensions/set-imports/imports/third-import-file.less`
       ])
         .lengthOf(9);
     });
 
-    it("Return Array", async () => {
-      let result = await lessHashed(less_files_path, hash_file_path, options);
+    it("return Array", async () => {
+      let result = await lessHashed(less_files_path);
       assert.isArray(result, 'Index must return Array');
     });
-  });
 
-  context('When save_sources_hashes_file is true', () => {
-    let options = {
-      save_hash_file: true
-    };
-
-    beforeEach(async () => {
-      await lessHashed(less_files_path, hash_file_path, options);
-    });
-
-    it("save hash file on disc", async () => {
-      fs.existsSync(hash_file_path).should.be.true();
-      fs.unlinkSync(hash_file_path);
-      fs.existsSync(hash_file_path).should.be.false();
+    it("does not save the file with hashes to disk", () => {
+      fs.existsSync(hash_file_path + hash_file_name).should.be.false();
     });
 
   });
+
+  describe('With options', () => {
+    context('With hashPath and without hashName', () => {
+
+      let options = {
+        hashPath: hash_file_path
+      };
+
+      beforeEach(async () => {
+        await lessHashed(less_files_path, options);
+      });
+
+      it("save hash file on disc", () => {
+        fs.existsSync(hash_file_path + hash_file_name).should.be.true();
+        fs.unlinkSync(hash_file_path + hash_file_name);
+        fs.existsSync(hash_file_path + hash_file_name).should.be.false();
+      });
+
+      it("hash name must be hashes.json", () => {
+        fs.existsSync(hash_file_path + hash_file_name).should.be.true();
+        fs.unlinkSync(hash_file_path + hash_file_name);
+        fs.existsSync(hash_file_path + hash_file_name).should.be.false();
+      });
+
+    });
+
+    context('With hashPath and hashName', () => {
+
+      const hash_file_name = 'test.json';
+      let options = {
+        hashPath: hash_file_path,
+        hashName: hash_file_name
+      };
+
+      beforeEach(async () => {
+        await lessHashed(less_files_path, options);
+      });
+
+      it("save hash file on disc", () => {
+        fs.existsSync(hash_file_path + hash_file_name).should.be.true();
+        fs.unlinkSync(hash_file_path + hash_file_name);
+        fs.existsSync(hash_file_path + hash_file_name).should.be.false();
+      });
+
+      it("hash name must be hashName", () => {
+        fs.existsSync(hash_file_path + hash_file_name).should.be.true();
+        fs.unlinkSync(hash_file_path + hash_file_name);
+        fs.existsSync(hash_file_path + hash_file_name).should.be.false();
+      });
+
+    });
+
+  });
+
 });
